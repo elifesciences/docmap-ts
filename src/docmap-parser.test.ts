755b0513ac1a5df5b0bb7e33686e01e7d88bb9a0
Scott Aubrey <scott@aubrey.org.uk>
2022-12-07 16:33:57 +0000

    add jest for testing

---
+++ src/docmap-parser.test.ts
@@ -0,0 +1,8 @@
+describe('docmap-parser', () => {
+  it.todo('finds a preprint from a docmap');
+  it.todo('finds reviews and editor evaluations from a docmap');
+  it.todo('finds a revised preprint from a docmap');
+  it.todo('finds a revised preprint reviews and evaluations from a docmap');
+  it.todo('finds a revised preprint evaluations, but no new reviews from a docmap');
+  it.todo('finds a revised preprint evaluations, but no new reviews from a docmap');
+})
