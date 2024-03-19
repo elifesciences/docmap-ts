658de90ff60b809fc241e5078f0e3c7b1557b54b
Nathan Lisgo <nathan@lisgo.co.uk>
2024-03-19 10:42:43 +0000

    feat: update fetch URL and limit docmaps iteration
    Update the fetch URL to use version 2 of the API. Also, limit the iteration of docmaps to the first 3 entries for demonstration purposes. Remove the process exit command.

--- src/cli/fetch-and-parse.ts
+++ src/cli/fetch-and-parse.ts
@@ -1,12 +1,12 @@
 /* eslint-disable no-console */
-import { exit } from 'process';
 import { DocMap } from '../types';
 import { parsePreprintDocMap } from '../parser/docmap-parser';
 
-fetch('https://data-hub-api--stg.elifesciences.org/enhanced-preprints/docmaps/v1/index')
+fetch('https://data-hub-api--stg.elifesciences.org/enhanced-preprints/docmaps/v2/index')
   .then((data) => data.json())
   .then((data) => {
-    data.docmaps.forEach((docMapStruct: DocMap) => {
+    // Limiting to 3 for demonstration purposes.
+    data.docmaps.slice(0, 3).forEach((docMapStruct: DocMap) => {
       const docMapEditableStruct = JSON.parse(JSON.stringify(docMapStruct));
       // You can make edits here
 
@@ -14,6 +14,5 @@ fetch('https://data-hub-api--stg.elifesciences.org/enhanced-preprints/docmaps/v1
       const parsedDocMap = parsePreprintDocMap(docMapJson);
       console.log(JSON.stringify(docMapEditableStruct, undefined, '  '));
       console.log(JSON.stringify(parsedDocMap, undefined, '  '));
-      exit(1);
     });
   });
